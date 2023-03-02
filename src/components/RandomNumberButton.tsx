178e921a8dd9f707297446acc9773f7442e01d28
Marvin <marvinvillalon23@gmail.com>
2023-03-02 19:58:13 +0800

    nextjs files added

---
+++ src/components/RandomNumberButton.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import styles from '@/styles/Home.module.css'
+
+
+const RandomNumberButton = () => {
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  const generateRandomNumber = () => {
+    const interval = setInterval(() => {
+        const newRandomNumber = Math.floor(Math.random() * 100) + 1;
+        setRandomNumber(newRandomNumber);
+    },50);
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 2000);
+  };
+
+  return (
+    <div className={styles.main}>
+      {randomNumber}
+      <button onClick={generateRandomNumber} className={styles.button}>Generate Random Number</button>
+    </div>
+  );
+};
+
+export default RandomNumberButton;
